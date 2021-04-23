c8dd563e90b245a0af0ecf9a34534d5a578c22a4
Sergio Valdés Valdés <60680494+svcu@users.noreply.github.com>
2021-04-22 23:35:22 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 
 var mongoose = require('mongoose');
 //Set up default mongoose connection
-var mongoDB = "mongodb://localhost/starhub-db";
+var mongoDB = "mongodb://uy25ujhmmnnzpzpm5f8l:zEitlPv3Akks3wuLrog0@bv6fil2o8mkqsir-mongodb.services.clever-cloud.com:27017/bv6fil2o8mkqsir";
 mongoose.connect(mongoDB, { useNewUrlParser: true });
  //Get the default connection
 var db = mongoose.connection;
